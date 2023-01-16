45d7ce1d35b952aa652886e5a5cf02eebb43b3e5
troestercole <30329688+troestercole@users.noreply.github.com>
2023-01-16 11:58:24 -0600

    Preloader

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+$(window).on('load', (function() {
+    $('.preloader').fadeOut('slow');
+ }));
+
 $('.socials-opener').click(() => {
     $('.socials').toggleClass('right');
     $('.socials-opener').toggleClass('right');
